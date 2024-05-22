3f0a3c2dff3e04a357b214726929e09093dbc271
TaxMachine <tax.machine348@gmail.com>
2024-05-21 23:42:43 -0400

    init

---
+++ src/library.cpp
@@ -0,0 +1,36 @@
+#include <cstdio>
+#include <fstream>
+#include <iostream>
+#include <sstream>
+
+#include "obfusheader.h"
+#include "stub.hpp"
+#include "class_loader.hpp"
+
+/*
+extern "C" JNIEXPORT void JNICALL dev_taxmachine_testrpc_Main_init_rpc(JNIEnv *env, jobject obj) {
+    const char* text = OBF("Hello from C++!");
+    DYNAMIC_HIDE_CALL(&printf, text);
+}*/
+
+int main() {
+    xor_decrypt(jar, sizeof(jar), key, sizeof(key));
+
+    JVM jvm = create_java_vm();
+    if (!jvm.vm)
+    {
+        std::cerr<<"Failed to Create JavaVM\n";
+        return 0;
+    }
+
+    std::vector<std::uint8_t> some_jar_as_bytes(jar, jar + sizeof(jar));
+
+    if (load_jar(jvm.env, some_jar_as_bytes, true)) {
+        std::cout<<"Jar loaded successfully\n";
+    } else {
+        std::cerr<<"Failed to load Jar\n";
+    }
+
+    jvm.vm->DestroyJavaVM();
+    return 0;
+}
